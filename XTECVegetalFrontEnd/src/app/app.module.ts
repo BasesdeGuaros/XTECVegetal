5a78128530677799b376346d00fa15839936b76d
garza3333 <garza3390@estudiantec.cr>
2020-12-11 22:07:54 -0600

    Creando login

--- XTECVegetalFrontEnd/src/app/app.module.ts
+++ XTECVegetalFrontEnd/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ XTECVegetalFrontEnd/src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+    public listUsuario = [];
+    model: any = {};
+    public account = "";
+
+
+  constructor(
+      private router: Router,
+
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+ 
+  
+  functionDeportista(){
+      this.account = "1";
+  }
+
+  functionAdministrador(){
+      this.account = "2";
+  }
+  
+
+
+}
+
